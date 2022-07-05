3ffae1ee7c22ed0c3cc0889f84d2aa20e695f119
Diana Linkova <di_rome@mail.ru>
2022-07-05 18:10:13 +0400

    feat: add reverse-int code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = n + ""; // convert number to string
+    let b = n.split("").slice(1).reverse().join("");
+    let c = n.split("").reverse().join("");
+    if (n < 0) {
+        return b;
+    } else {
+        return c;
+    }
+};
